8bcc170d80b9930ee80cae2e9f7a85316a569a3c
Oscar Aguilar <aguilargonzalezoscar15@gmail.com>
2022-12-18 22:53:06 -0600

    novedades con cambio de imagen listo

--- public/html/promociones.php
+++ public/html/promociones.php
@@ -1,13 +1,45 @@
 <?php include 'partials/header.php'; ?>
+    
+    <section class="promociones" >
+        <div class="promo-container">
+            <div class="prom-list">    
+                
+                    <div class="prom-card-container">
+                        <div class="prom-card prom-card-active">
+                            <div class="prom-card-text">
+                                <h3>Title promotion 1</h3>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                Temporibus consequatur, ullam repellendus culpa pariatur ut.</p>
+                            </div>
+                            <img src="../images/test.png" class="prom-card-image">
+                        </div>    
+                    </div>
+                    
+                    <div class="prom-card-container">
+                        <div class="prom-card">
+                            <div class="prom-card-text">
+                                <h3>Title promotion 2</h3>
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                                Temporibus consequatur, ullam repellendus culpa pariatur ut.</p>
+                            </div>
+                            <img src="../images/chems-icon.png" class="prom-card-image">
+                        </div>    
+                    </div>
+            </div>
+            
+            <div class="prom-visualizer">
+                <div class="prom-visual-container">
+                    <img src="../images/test.png" class="big-image">
+                    <div class="prom-visual-text-container">
+                        <h3>Promotion 1</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Repudiandae temporibus accusamus, dolore eius ad nisi.</p>
+                    </div>
+                </div>
 
-    <div class="promo-container">
-        <div class="prom-list">
-        
+            </div>
         </div>
-
-        <div class="prom-visualizer">
-        
-        </div>
-    </div>
+    </section>
+    <script src="../javascripts/image_selector.js"></script>
 
 <?php include 'partials/footer.php'; ?>
